b25479b9209daf8d82b9f61fb8c09828982122a6
boretsky8000 <borekirill@gmail.com>
2020-08-26 17:16:00 +0300

    Подключено бутстрап и джкуєри

--- generate_index.py
+++ generate_index.py
@@ -13,6 +13,7 @@ def generate_page(head, body):
 def generate_head(title):
     head = "<head><meta charset=\'utf-8\'>"
     head += "<title>" + title + "</title>"
+    head += """<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" /><script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>"""
     head += "</head>"
     return head
 
